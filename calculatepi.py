b46838b4321067ea733974299cf44f5079f471a1
ryankynor <ryankynor@hanovernorwichschools.org>
2015-09-16 13:58:02 -0400

    Updated from Brython Server: September 16, 2015 1:58:02 PM EDT

--- calculatepi.py
+++ calculatepi.py
@@ -31,4 +31,3 @@ decimals = int(input("How many decimal places should I use in the result? "))
 pi = 4.0*sum([(((-1.0)**k)/(2*k+1)) for k in range(0,n)])
 
 print("The approximate value of pi is {0:.{1}f}".format(pi, decimals))
-print("(The true value of pi is {0:.{1}f})".format(math.pi, decimals))
